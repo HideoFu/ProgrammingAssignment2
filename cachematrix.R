f972377e47b0a1af3fb90bae9e78e40e3aa1a92d
HideoFu <h.fushimi.x689@gmail.com>
2015-02-23 04:44:34 +0900

    Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  To refer cache, some "global" variable is require.
+##  Such global variable: array "A" is a special "matrix". 
+##  Its first plane is original matrix, and second plane is inversed matrix.
 
-## Write a short comment describing this function
+##  To make the array, dimensions of the original matrix is obtained.
+##  Then global variable A is set with NULL second plane.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()){
 
+    rows <- nrow(x)
+    cols <- ncol(x)
+    
+    A <<- array(,dim=c(rows,cols,2))
+    A[,,1]<<-x
+    A[,,2]<<-NULL
 }
 
 
-## Write a short comment describing this function
+##  Call global variable A, and check if the second plane has values.
+##  If the second plane includes NA, then the value will be calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+    iv <- A[,,2]
+    
+    if(!anyNA(iv)){
+        message("getting cached value")
+        return(iv)
+    }
+    
+    iv <- solve(x, ...)
+    A[,,2]<<-iv
+    iv
 }
